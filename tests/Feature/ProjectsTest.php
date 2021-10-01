a9089e521a70c75ff72c537d8e39864c827a4e34
Igor Alves <igor.alves@lessclick.com>
2021-10-01 16:30:38 -0300

    criando o primeiro caso de teste - crud de projetos

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -8,15 +8,22 @@ use Tests\TestCase;
 
 class ProjectsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use WithFaker, RefreshDatabase;
+
+    public function test_a_user_can_create_a_project(){
+
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
     }
 }
